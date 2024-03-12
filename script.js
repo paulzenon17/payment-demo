02e8fd029ed827c7dd08ecf39a19f03a3aee72f4
paulzenon17 <96942377+paulzenon17@users.noreply.github.com>
2024-03-12 15:24:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function payNow(paymentMethod) {
     // Open a new tab with payment successful message
     const paymentWindow = window.open('', '_blank');
     paymentWindow.document.write('<html><head><title>Payment Status</title></head><body>');
-    paymentWindow.document.write('<img src="icon.jpg" width="300px" height="300px">');
+    paymentWindow.document.write('<img src="paid.jpg" width="300px" height="300px">');
     paymentWindow.document.write('<h2>$----Payment Successful----$</h2>');
     paymentWindow.document.write('<p>Your ' + paymentMethod + ' payment was successful.</p>');
     paymentWindow.document.write('</body></html>');
